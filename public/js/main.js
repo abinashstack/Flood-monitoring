8e4d919107ddcb710c8324234f332d25f6b1f8a1
abinashstack <abinash.gogoi55@gmail.com>
2020-11-24 15:13:52 +0530

    4

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+$(function() {
+"use strict";
+    
+    $('#nav').singlePageNav();
+    
+
+});
+
+(function($) {
+	'use strict';
+
+	var nav_offset_top = $('header').height() + 50;
+	/*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed
+	function navbarFixed() {
+		if ($('.header_area').length) {
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header_area').addClass('navbar_fixed');
+				} else {
+					$('.header_area').removeClass('navbar_fixed');
+				}
+			});
+		}
+	}
+	navbarFixed();
+
+	/*----------------------------------------------------*/
+	/*  MailChimp Slider
+    /*----------------------------------------------------*/
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+	mailChimp();
+
+	$('select').niceSelect();
+	/* ---------------------------------------------
+            Isotope js Starts
+         --------------------------------------------- */
+	$(window).on('load', function() {
+		$('.portfolio-filter ul li').on('click', function() {
+			$('.portfolio-filter ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$workGrid.isotope({
+				filter: data
+			});
+		});
+
+		if (document.getElementById('portfolio')) {
+			var $workGrid = $('.portfolio-grid').isotope({
+				itemSelector: '.all',
+				percentPosition: true,
+				masonry: {
+					columnWidth: '.all'
+				}
+			});
+		}
+    });
+
+
